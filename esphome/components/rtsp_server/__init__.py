9407a3309c4ea7a28d267e527d030420213ab5ef
sujal <sujal@var.io>
2022-02-10 22:45:54 -0500

    declare CONF_CAMERA locally as a workaround
    This allows this branch to be used as an external component.

--- esphome/components/rtsp_server/__init__.py
+++ esphome/components/rtsp_server/__init__.py
@@ -1,8 +1,9 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.const import CONF_ID, CONF_PORT, CONF_CAMERA
+from esphome.const import CONF_ID, CONF_PORT
 from esphome.components import esp32_camera
 
+CONF_CAMERA = "camera"
 
 CODEOWNERS = ["@crossan007"]
 DEPENDENCIES = ["network", "esp32_camera"]
